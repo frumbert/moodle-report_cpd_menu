99191e43499f25afd7aefac63df8b332b3cdce3f
Tim St.Clair <tim.stclair@gmail.com>
2012-04-23 18:34:27 +1000

    Fixed $DB references, various depreciated values

---
+++ edit_activity.php
@@ -0,0 +1,115 @@
+<?php
+// This file is part of CPD Report for Moodle
+//
+// CPD Report for Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CPD Report for Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with CPD Report for Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * This page is used to add or edit CPD Activities.
+ *
+ * @package   admin-report-cpd
+ * @copyright 2010 Kineo open Source
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('../../config.php');
+require_once($CFG->libdir.'/adminlib.php');
+require_once($CFG->libdir.'/formslib.php');
+require_once('edit_activity_form.php');
+require_once('lib.php');
+
+// register globals
+global $DB;
+require_login($SITE);
+$PAGE->set_url('/report/cpd/edit_activity.php');
+$PAGE->set_context(get_context_instance(CONTEXT_SYSTEM));
+$output = $PAGE->get_renderer('report_cpd');
+
+$cpdyearid = required_param('cpdyearid', PARAM_INT);
+$cpdid = optional_param('id', null, PARAM_INT);
+$cpd_record = null;
+
+$redirecturl = new moodle_url('/report/cpd/index.php', array('cpdyearid' => $cpdyearid)); /* Redirect back to user CPDs */
+
+if (!empty($cpdyearid)) {
+    if (! $cpdyear = $DB->get_record('cpd_year', array('id' => $cpdyearid))) { //get cpd year start and end
+        print_error('invalidcpdyear', 'report_cpd');
+    }
+}
+
+if (!empty($cpdid)) {
+    if (! $cpd_record = $DB->get_record('cpd', array('id' => $cpdid))) {
+        print_error('invalidcpdactivity', 'report_cpd');
+    }
+}
+
+//get data
+$activity_types = get_cpd_menu('activity_types');
+$statuses = get_cpd_menu('statuses');
+
+$mform = new edit_activity_form("edit_activity.php", compact('activity_types', 'statuses', 'cpdid', 'cpdyearid', 'cpdyear'));
+$frmdata = $mform->get_data();
+
+if (!empty($frmdata)) {
+
+    $errors = process_activity_form($frmdata, $redirecturl->out());
+
+} else if ($cpd_record) {
+
+    $cpd_record = (array)$cpd_record;
+
+    if ($cpd_record['duedate']) {
+        $cpd_record['duedate[d]'] = date('d', $cpd_record['duedate']);
+        $cpd_record['duedate[m]'] = date('m', $cpd_record['duedate']);
+        $cpd_record['duedate[Y]'] = date('Y', $cpd_record['duedate']);
+        unset($cpd_record['duedate']);
+    }
+
+    if ($cpd_record['startdate']) {
+        $cpd_record['startdate[d]'] = date('d', $cpd_record['startdate']);
+        $cpd_record['startdate[m]'] = date('m', $cpd_record['startdate']);
+        $cpd_record['startdate[Y]'] = date('Y', $cpd_record['startdate']);
+        unset($cpd_record['startdate']);
+    }
+
+    if ($cpd_record['timetaken']) {
+        $cpd_record['timetaken[minutes]'] = $cpd_record['timetaken'] % 60;
+        $cpd_record['timetaken[hours]'] = ($cpd_record['timetaken'] - $cpd_record['timetaken[minutes]']) / 60;
+        unset($cpd_record['timetaken']);
+    }
+
+    $mform->set_data($cpd_record);
+
+} else {
+    //Set due and start dates to today
+    $dates['duedate[d]'] = date('d', $cpdyear->enddate);
+    $dates['duedate[m]'] = date('m', $cpdyear->enddate);
+    $dates['duedate[Y]'] = date('Y', $cpdyear->enddate);
+    $dates['startdate[d]'] = date('d');
+    $dates['startdate[m]'] = date('m');
+    $dates['startdate[Y]'] = date('Y');
+    $mform->set_data($dates);
+}
+// Print the header.
+admin_externalpage_setup('cpdrecord');
+// Include styles
+echo $OUTPUT->header();
+
+if (isset($errors)) {
+    $output->error_box($errors);
+}
+
+$mform->display();
+
+echo $OUTPUT->footer();
---
+++ metadata.php
@@ -0,0 +1,86 @@
+<?php
+// This file is part of CPD Report for Moodle
+//
+// CPD Report for Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CPD Report for Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with CPD Report for Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * This page is used to add, modify or delete CPD Years, Activity types, or Statuses.
+ *
+ * You can also change the display order of the Statuses.
+ * You cannot delete or modify status 'Objective Met', because End Date of a CPD Activity is set when
+ * the status is changed to this.
+ *
+ * @package   admin-report-cpd
+ * @copyright 2010 Kineo open Source
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('../../config.php');
+require_once($CFG->libdir.'/adminlib.php');
+require_once('lib.php');
+
+// rgister globals
+global $DB;
+
+// Check permissions.
+require_login($SITE);
+$systemcontext = get_context_instance(CONTEXT_SYSTEM);
+require_capability('report/cpd:superadminview', $systemcontext);
+$PAGE->set_url($CFG->dirroot.'/report/cpd/metadata.php');
+$PAGE->set_context(get_context_instance(CONTEXT_SYSTEM));
+$output = $PAGE->get_renderer('report_cpd');
+
+$id = optional_param('id', NULL, PARAM_INT);
+$errors = null;
+$edit = null;
+
+
+if ($process = optional_param('process', null, PARAM_RAW)) {
+    $errors = process_meta_form($process);
+} else if ($table = optional_param('delete', null, PARAM_RAW)) {
+    delete_meta_record($table, $id);
+} else if ($table = optional_param('edit', null, PARAM_RAW)) {
+    if ($result = get_meta_records($table, $id)) {
+        $edit[$table] = $result;
+    }
+} else if ($table = optional_param('moveup', null, PARAM_RAW)) {
+    change_display_order($table, $id, 'up');
+} else if ($table = optional_param('movedown', null, PARAM_RAW)) {
+    change_display_order($table, $id, 'down');
+}
+
+$activity_types = $DB->get_records('cpd_activity_type', null,'name asc','*');
+$years = $DB->get_records('cpd_year', null,'startdate asc, enddate asc','*');
+$statuses = $DB->get_records('cpd_status', null,'display_order asc','*');
+
+// Print the header.
+admin_externalpage_setup('cpdmetadata');
+echo $OUTPUT->header();
+
+global $CFG;
+if (isset($errors)) {
+    echo $output->error_box($errors);
+}
+$editactivitytype = isset($edit['activitytype']) ? $edit['activitytype'] : null;
+echo $output->settings_form('activitytype', $activity_types, $editactivitytype);
+
+$edityear = isset($edit['cpdyears']) ? $edit['cpdyears'] : null;
+echo $output->settings_form('cpdyears', $years, $edityear, true);
+
+$editstatus = isset($edit['status']) ? $edit['status'] : null;
+echo $output->settings_form('status', $statuses, $editstatus, false, true);
+
+echo $OUTPUT->footer();
+?>
